b94605f2c12efe95dffaa7498ee18fdc88911d34
Sisypheus <contact@sisypheus.dev>
2024-05-01 21:17:07 +0200

    feat: updated file listing style

--- src/components/list-files.tsx
+++ src/components/list-files.tsx
@@ -12,19 +12,19 @@ export function List({ children }: PropsWithChildren) {
 export function RecentFile({ name, size, id }: PlumeFile) {
   if (!name.endsWith('.plm')) throw new Error('Invalid file extension');
   
-  return <Link href="/files/[slug]" as={`/files/${id}`} className="border border-neutral-300 p-4 rounded-lg px-6 grid grid-cols-3 items-center">
-    <div className="inline-flex items-center gap-2">
-      <h3 className="text-lg font-mono text-neutral-800">
+  return <Link href="/files/[slug]" as={`/files/${id}`} className="border border-white/15 p-4 rounded-lg px-6 grid grid-cols-3 items-center">
+    <div className="inline-flex items-center gap-2 col-span-2">
+      <h3 className="text-lg font-mono text-white/90">
         {name}
       </h3>
       <span className="text-neutral-400">-</span>
-      <span className="text-sm font-medium text-neutral-800/70">
+      <span className="text-sm font-medium text-white/70">
         {prettySize(size)}
       </span>
     </div>
 
-    <div className="col-span-2 justify-end grid">
-      <DotsHorizontalIcon className="text-neutral-800/50 h-5 w-5" />
+    <div className="col-span-1 justify-end grid">
+      <DotsHorizontalIcon className="text-white/50 h-5 w-5" />
     </div>
   </Link>;
 }
