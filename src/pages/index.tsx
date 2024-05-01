e6e2fa85aec5e4c8754ae2001a536619539fa85f
Sisypheus <contact@sisypheus.dev>
2024-05-01 21:16:22 +0200

    style: updated index style from light to dark

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,49 @@
-import { List, RecentFile } from "@/components/list-files";
-import { PlumeFile } from "@/file";
-import { v4 } from "uuid";
-import Image from "next/image";
+'use client'
 
-export function createFiles(n: number): PlumeFile[] {
-  return Array.from({ length: n }, (_, i) => ({
-    id: v4(),
-    name: `file-${i}.plm`,
-    size: 1096 * (i + 1),
-  }));
-}
+import { List, RecentFile } from "@/components/list-files";
+import { PlumeFile, dataValidation } from "@/file";
+import Image from "next/image";
+import { useIsClient, useLocalStorage } from "@uidotdev/usehooks";
+import { uniqueNamesGenerator, starWars, adjectives } from "unique-names-generator";
+import { Dispatch, SetStateAction } from "react";
 
 export default function Home() {
+  const isClient = useIsClient();
+
+  if (!isClient) return null;
+
+  return <HomePage />;
+}
+
+async function createFile(setFiles: Dispatch<SetStateAction<PlumeFile[]>>) {
+  const generated = uniqueNamesGenerator({
+    dictionaries: [adjectives, starWars],
+    length: 2,
+    style: 'lowerCase',
+    separator: ' ',
+  });
+  const fileName = generated.split(' ').join('-');
+
+  const res = await fetch('/api/create-file', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name: `${fileName}.plm` }),
+  });
+  
+  if (!res.ok) throw new Error('Failed to create file');
+
+  const data = dataValidation.parse(await res.json());
+
+  console.log(data);
+
+  setFiles(files => [data, ...files]);
+}
+
+function HomePage() {
+  const [files, setFiles] = useLocalStorage<PlumeFile[]>('files', []);
+
   return <>
     <header className="p-8 text-center max-w-5xl mx-auto mt-32">
       <Image
@@ -21,30 +53,35 @@ export default function Home() {
         width={128}
         height={128}
       />
-      <h1 className="text-4xl font-black mt-8 text-neutral-800">
+      <h1 className="text-4xl font-black mt-8 text-white">
         The Plume Playground
       </h1>
-      <p className="mt-4 text-lg font-mono max-w-xl mx-auto text-neutral-800/70">
+      <p className="mt-4 text-lg font-mono max-w-xl mx-auto text-white/70">
         Test Plume&lsquo;s latest features directly in your browser, with no need to install anything!
       </p>
     </header>
 
     <main className="max-w-3xl mx-auto mt-8">
       <nav className="flex flex-row items-center">
-        <span className="flex-initial text-neutral-800/50">
+        <span className="flex-initial text-white/50">
           Recently edited files
         </span>
         
         <ul className="flex flex-auto items-center justify-end">
-          <button className="bg-hot-pink py-2 px-6 text-lg rounded-lg font-medium text-white focus:outline-none">
+          <button className="bg-hot-pink-400 py-2 px-6 text-lg rounded-lg font-medium text-white focus:outline-none" onClick={() => createFile(setFiles)}>
             Start coding
           </button>
         </ul>
       </nav>
 
-      <List>
-        {createFiles(5).map(file => <RecentFile key={file.id} {...file} />)}
-      </List>
+      {files.length > 0
+        ? <List>
+            {files.map(file => <RecentFile key={file.id} {...file} />)}
+          </List>
+        : <p className="text-white/70 mt-16 text-lg text-center">
+            No files have been edited yet.
+          </p>
+      }
     </main>
   </>;
 }
