b749083ac57854a953afd62f0e45e1a3d1ee2042
Sisypheus <contact@sisypheus.dev>
2024-05-04 22:28:45 +0200

    feat: added lightweight terminal renderer

---
+++ playground-frontend/src/components/terminal.tsx
@@ -0,0 +1,49 @@
+import { tw } from "#root/library/tailwind";
+import { useWindowSize } from "@uidotdev/usehooks";
+import Convert from "ansi-to-html";
+import { MutableRefObject } from "react";
+
+interface RenderTerminalProps {
+  terminalRef: MutableRefObject<HTMLDivElement | null>;
+  content: string;
+}
+
+function setupANSI() {
+  const convert = new Convert({
+    colors: [
+      tw.colors.zinc["950"],
+      tw.colors.red["600"],
+      tw.colors.green["500"],
+      tw.colors.yellow["600"],
+      tw.colors.blue["600"],
+      tw.colors.fuchsia["600"],
+      tw.colors.cyan["600"],
+      tw.colors.zinc["300"],
+      tw.colors.zinc["700"],
+      tw.colors.red["400"],
+      tw.colors.green["400"],
+      tw.colors.yellow["400"],
+      tw.colors.blue["400"],
+      tw.colors.pink["400"],
+      tw.colors.cyan["400"],
+      tw.colors.white,
+    ]
+  });
+
+  return convert;
+}
+
+export function RenderTerminal({ content, terminalRef }: RenderTerminalProps) {
+  const convert = setupANSI();
+  const converted = convert.toHtml(content);
+  const size = useWindowSize();
+
+  return <div
+    className="w-full h-full bg-zinc-900 text-zinc-200 font-mono whitespace-break-spaces px-8 overflow-y-auto"
+    ref={terminalRef} 
+    style={{
+      height: size.height ? size.height / 3 : '100%'
+      
+    }}
+    dangerouslySetInnerHTML={{ __html: converted }} />;
+}
